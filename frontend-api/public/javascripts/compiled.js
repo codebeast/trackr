f0d8570c33365fb674e0a7bfa689d353c10ec276
jeffreya <andrew-jeffrey@live.co.uk>
2016-10-11 15:23:26 +0100

    added files

---
+++ frontend-api/public/javascripts/compiled.js
@@ -0,0 +1,1 @@
+console.error("ReactifyError: /Users/jeffreya/workspace/trackr/frontend-api/assets/javascripts/components/App.js: Parse Error: Line 1: Illegal import declaration while parsing file: /Users/jeffreya/workspace/trackr/frontend-api/assets/javascripts/components/App.js");
