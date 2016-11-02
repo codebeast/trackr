1392a9b567cfc187c619020b413b936887448473
jeffreya <andrew-jeffrey@live.co.uk>
2016-11-02 15:52:09 +0000

    refactoring

--- listener/src/main/java/com/javabeast/Application.java
+++ listener/src/main/java/com/javabeast/Application.java
@@ -1,19 +1,12 @@
-package com.objectpartners.udp;
+package com.javabeast;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import com.javabeast.udpserver.UDPServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import reactor.core.Environment;
-import reactor.io.encoding.StandardCodecs;
-import reactor.net.netty.udp.NettyDatagramServer;
-import reactor.net.tcp.support.SocketUtils;
-import reactor.net.udp.DatagramServer;
-import reactor.net.udp.spec.DatagramServerSpec;
+import org.springframework.context.annotation.Import;
 import reactor.spring.context.config.EnableReactor;
 
 import java.util.concurrent.CountDownLatch;
@@ -22,28 +15,8 @@ import java.util.concurrent.CountDownLatch;
 @EnableAutoConfiguration
 @EnableReactor
 @ComponentScan
+@Import(UDPServer.class)
 public class Application {
-
-    private Log log = LogFactory.getLog(Application.class);
-
-    @Bean
-    public DatagramServer<byte[], byte[]> datagramServer(Environment env) throws InterruptedException {
-
-        final DatagramServer<byte[], byte[]> server = new DatagramServerSpec<byte[], byte[]>(NettyDatagramServer.class)
-                .env(env)
-                .listen(SocketUtils.findAvailableTcpPort())
-                .codec(StandardCodecs.BYTE_ARRAY_CODEC)
-                .consumeInput(bytes -> log.info("received: " + new String(bytes)))
-                .get();
-
-        server.start().await();
-        return server;
-    }
-
-    @Bean
-    public CountDownLatch latch() {
-        return new CountDownLatch(1);
-    }
 
     public static void main(String[] args) throws InterruptedException {
         ApplicationContext ctx = SpringApplication.run(Application.class);
