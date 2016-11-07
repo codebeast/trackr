921e847cbe6dff0816d6ce75b8345ed13a20c963
jeffreya <andrew-jeffrey@live.co.uk>
2016-11-07 13:36:08 +0000

    Added trackrmessage

---
+++ domain/src/main/java/com/javabeast/TrackerMessage.java
@@ -0,0 +1,16 @@
+package com.javabeast;
+
+import java.util.Date;
+
+/**
+ * Created by jeffreya on 07/11/2016.
+ *
+ */
+
+public class TrackerMessage {
+
+    private Date timeStamp;
+    private byte [] data;
+
+
+}
