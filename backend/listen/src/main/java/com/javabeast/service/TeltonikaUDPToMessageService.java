f4465f8ed9de00eba708d7f192724feed4711ad7
jeffreya <andrew-jeffrey@live.co.uk>
2016-12-20 15:11:33 +0000

    added 1 2 4 8 byte

--- backend/listen/src/main/java/com/javabeast/service/TeltonikaUDPToMessageService.java
+++ backend/listen/src/main/java/com/javabeast/service/TeltonikaUDPToMessageService.java
@@ -7,6 +7,7 @@ import com.javabeast.teltonikia.UDPChannelHeader;
 import org.springframework.stereotype.Service;
 
 import javax.xml.bind.DatatypeConverter;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -88,52 +89,56 @@ public class TeltonikaUDPToMessageService {
     private List<AVLData> getAvlDatas(final int numOfRecords, final String data) {
         final String hexData = data.substring(4);
         final List<AVLData> avlDataList = new ArrayList<>();
-        for (int i = 0; i < numOfRecords; i++) {
-/*
 
-            00000113fc208dff – Timestamp in milliseconds (1185345998335 → 1185345998,335 in Unix Timestamp = 25 Jul 2007 06:46:38 UTC)
-            00 – Priority
-            GPS Element
-            0f14f650 – Longitude 253032016 = 25,3032016º N
-            209cca80 – Latitude 547146368 = 54,7146368 º E
-            006f – Altitude 111 meters
-            00d6 – Angle 214º
-            04 – 4 Visible sattelites
-            0004 – 4 km/h speed
-            IO Element
-            00 – IO element ID of Event generated (in this case when 00 – data generated not on event)
-                04 – 4 IO elements in record
-                03 – 3 IO elements, which length is 1 Byte
-                01 – IO element ID = 01
-                01 – 1’st IO element’s value = 1
-                15 – IO element ID = 21
-                03 – 21’st IO element’s value = 3
-                16 – IO element ID = 22
-                03 – 22’nd IO element’s value = 3
-                00 – 0 IO elements, which value length is 2 Bytes
-                01 – 1 IO element, which value length is 4 Bytes
-                46 – IO element ID = 70
-                5
-                0000015d – 70’th IO element’s value = 349
-                00 – 0 IO elements, which value length is 8 Bytes
-*/
+        int offset = 0;
 
-            final String timestampHex = hexData.substring(0, 16);
-            final int priority = Integer.parseInt(hexData.substring(16, 18), RADIX);
-            final String gpsHex = hexData.substring(18, 48);
-            final String ioHex = hexData.substring(48, 100);
-            System.out.println(ioHex);
+        for (int i = 0; i < 1; i++) {
+            final int start = offset;
 
+            int position = offset;
+            final String timestampHex = hexData.substring(position, position += 16);
+            final int priority = Integer.parseInt(hexData.substring(position, position += 2), RADIX);
+            final String gpsHex = hexData.substring(position, position += 30);
+            final String ioHex = hexData.substring(position);
+            //System.out.println(ioHex);
 
-            final int eventId = Integer.parseInt(ioHex.substring(0, 2));
-            final int ioEventsTotal = Integer.parseInt(ioHex.substring(2, 4));
-            final int oneByteEventTotal = Integer.parseInt(ioHex.substring(4, 6));
+            final int eventId = Integer.parseInt(hexData.substring(position, position += 2), RADIX);
+            final int ioEventsTotal = Integer.parseInt(hexData.substring(position, position += 2), RADIX);
+            final int oneByteEventTotal = Integer.parseInt(hexData.substring(position, position += 2), RADIX);
+
             for (int oneByteEventCount = 0; oneByteEventCount < oneByteEventTotal; oneByteEventCount++) {
-                final int elementId = Integer.parseInt(ioHex.substring(6, 8), RADIX);
-                System.out.println(elementId);
+                final int elementId = Integer.parseInt(hexData.substring(position, position += 2), RADIX);
+                final int elementValue = Integer.parseInt(hexData.substring(position, position += 2), RADIX);
+                System.out.println("1byte");
+                System.out.println(elementId + ":" + elementValue);
+            }
+
+            final int twoByteEventTotal = Integer.parseInt(hexData.substring(position, position += 2), RADIX);
+            for (int twoByteEventCount = 0; twoByteEventCount < twoByteEventTotal; twoByteEventCount++) {
+                final int elementId = Integer.parseInt(hexData.substring(position, position += 2), RADIX);
+                final int elementValue = Integer.parseInt(hexData.substring(position, position += 4), RADIX);
+                System.out.println("2byte");
+                System.out.println(elementId + ":" + elementValue);
+            }
+
+            final int fourByteEventTotal = Integer.parseInt(hexData.substring(position, position += 2), RADIX);
+            for (int fourByteEventCount = 0; fourByteEventCount < fourByteEventTotal; fourByteEventCount++) {
+                final int elementId = Integer.parseInt(hexData.substring(position, position += 2), RADIX);
+                final int elementValue = Integer.parseInt(hexData.substring(position, position += 8), RADIX);
+                System.out.println("4byte");
+                System.out.println(elementId + ":" + elementValue);
+            }
+
+            final int eightByteEventTotal = Integer.parseInt(hexData.substring(position, position += 2), RADIX);
+            for (int eightByteEventCount = 0; eightByteEventCount < eightByteEventTotal; eightByteEventCount++) {
+                final int elementId = Integer.parseInt(hexData.substring(position, position += 2), RADIX);
+                final int elementValue = Integer.parseInt(hexData.substring(position, position += 16), RADIX);
+                System.out.println("8byte");
+                System.out.println(elementId + ":" + elementValue);
             }
 
 
+            offset += (position - start);
 //            Timestamp 8
 //            Priority 1
 //            GPS Element 15
