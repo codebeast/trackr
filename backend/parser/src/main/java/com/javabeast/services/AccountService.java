12d35f7f419dd413c762156398dea73485b7b05a
jeffreya <andrew-jeffrey@live.co.uk>
2017-01-07 13:20:16 +0000

    removed imports

--- backend/parser/src/main/java/com/javabeast/services/AccountService.java
+++ backend/parser/src/main/java/com/javabeast/services/AccountService.java
@@ -1,11 +1,6 @@
 package com.javabeast.services;
 
-import com.javabeast.account.Account;
-import com.javabeast.account.CreateAccount;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Service
 public class AccountService {
