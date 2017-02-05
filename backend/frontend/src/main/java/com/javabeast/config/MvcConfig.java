f5746f7251468994991a8cb6ef899260731fb12d
jeffreya <andrew-jeffrey@live.co.uk>
2017-02-05 10:02:24 +0000

    Added parralax framework

--- backend/frontend/src/main/java/com/javabeast/config/MvcConfig.java
+++ backend/frontend/src/main/java/com/javabeast/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.javabeast.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -13,6 +14,13 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
     }
 
 }
--- backend/frontend/src/main/java/com/javabeast/config/WebSecurityConfig.java
+++ backend/frontend/src/main/java/com/javabeast/config/WebSecurityConfig.java
@@ -14,7 +14,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/", "/home", "/stylesheets/*", "/javascripts/*", "/images/*" , "/fonts/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
