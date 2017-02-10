9a8904e6bb6dfeb85ac08eb5b3350ea261ac43cc
jeffreya <andrew-jeffrey@live.co.uk>
2017-02-10 19:52:58 +0100

    updated app props to use hosts added test function added docker compose

--- backend/listen/src/test/java/com/javabeast/service/TestPort.java
+++ backend/listen/src/test/java/com/javabeast/service/TestPort.java
@@ -1,4 +1,29 @@
 package com.javabeast.service;
 
+
+import java.io.IOException;
+import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class TestPort {
+
+    private static final String MESSAGE_FILE_PATH = "/Users/jeffreya/workspace/trackr/backend/listen/src/test/resources/tracker_message.dat";
+    private static byte[] data;
+
+    static {
+        try {
+            final Path path = Paths.get(MESSAGE_FILE_PATH);
+            data = Files.readAllBytes(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        final DatagramSocket datagramSocket = new DatagramSocket(4999);
+        final DatagramPacket datagramPacket = new DatagramPacket(data, data.length, InetAddress.getByName("localhost"), 5000);
+        datagramSocket.send(datagramPacket);
+    }
 }
