c5050ff0f6e009d0e63d090b9e38d972b8734be5
jeffreya <andrew-jeffrey@live.co.uk>
2017-02-17 18:25:20 +0100

    Adding payment route to checkout

--- backend/frontend/src/main/java/com/javabeast/config/WebSecurityConfig.java
+++ backend/frontend/src/main/java/com/javabeast/config/WebSecurityConfig.java
@@ -14,7 +14,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home", "/faq", "/stylesheets/*", "/javascripts/**", "/images/*", "/font/*", "/fonts/*", "/templates/**", "/whereisfogie/*", "/createaccount").permitAll()
+                .antMatchers("/", "/home", "/faq", "/checkout",
+                        "/stylesheets/*", "/javascripts/**", "/images/*", "/font/*", "/fonts/*", "/templates/**", "/whereisfogie/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
