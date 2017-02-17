13a066c9e3dccac85876df570f1e2fd72e443f00
jeffreya <andrew-jeffrey@live.co.uk>
2017-02-17 11:33:58 +0100

    Added more languages Proper display names lang choice dropdown Added new images added layout for features added layout for application image added layout for how it works added company blurb section

--- backend/frontend/src/main/java/com/javabeast/domain/Language.java
+++ backend/frontend/src/main/java/com/javabeast/domain/Language.java
@@ -1,28 +1,43 @@
 package com.javabeast.domain;
 
 public enum Language {
-    EN("en"),
-    ES("es"),
-    DE("de"),
-    IT("it"),
-    NL("nl"),
-    PL("pl"),
-    PT("pt"),
-    DA("da"),
-    UK("uk"),
-    RU("ru"),
-    NO("no"),
-    SV("sv");
+    EN("en", "English"),
+    DE("de", "Deutsche"),
+    ES("es", "Español"),
+    IT("it", "Italiano"),
+    NL("nl", "Nederlands"),
+    PL("pl", "Polskie"),
+    PT("pt", "Português"),
+    DA("da", "Dansk"),
+    RU("ru", "русский"),
+    UK("uk", "український"),
+    NO("no", "Norsk"),
+    SV("sv", "Svenska");
 
     private final String displayName;
+    private final String languageName;
 
-    Language(String displayName) {
+    Language(String displayName, String languageName) {
         this.displayName = displayName;
+        this.languageName = languageName;
     }
 
     public String getDisplayName() {
         return displayName;
     }
 
+    public String getLanguageName() {
+        return languageName;
+    }
 
+    public static String getNameFromCode(final String langCode) {
+        final String isoCode = langCode.substring(0, 2);
+        final Language[] values = Language.values();
+        for (final Language value : values) {
+            if (value.getDisplayName().equals(isoCode)) {
+                return value.getLanguageName();
+            }
+        }
+        return Language.EN.getLanguageName();
+    }
 }
